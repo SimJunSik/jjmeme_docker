92df03a59dd6621881662c9c9abb6c1eb74fb868
JunSik Sim <wnstlr24@gmail.com>
2023-01-01 06:32:22 +0900

    first commit

---
+++ fastapi_project/main.py
@@ -0,0 +1,308 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, status
+from requests_aws4auth import AWS4Auth
+from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+from opensearchpy import OpenSearch, RequestsHttpConnection
+from typing import List
+from collections import Counter
+from loguru import logger
+
+import os
+
+
+app = FastAPI()
+logger.add("logs/search_log_{time}", rotation="12:00", compression="zip")
+templates = Jinja2Templates(directory="./templates/")
+
+load_dotenv(dotenv_path="./secrets/.env")
+
+
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
+AWS_REGION = os.environ.get("AWS_REGION")
+AWS_SERVICE = os.environ.get("AWS_SERVICE")
+
+HOST = os.environ.get("HOST")
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+awsauth = AWS4Auth(
+    AWS_ACCESS_KEY,
+    AWS_SECRET_KEY,
+    AWS_REGION,
+    AWS_SERVICE,
+)
+
+es = OpenSearch(
+    hosts=[{"host": HOST, "port": 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection,
+    timeout=30,
+    max_retries=10,
+    retry_on_timeout=True,
+)
+
+print(es.info())
+
+
+class Meme(BaseModel):
+    id: int = Field(title="RDS id")
+    title: str = Field(title="제목")
+    image_url: str = Field(title="이미지 URL")
+    image_width: int = Field("이미지 가로 길이")
+    image_height: int = Field("이미지 세로 길이")
+    tags: List[str] = Field(title="태그 목록")
+    view_count: int = Field(title="조회수")
+    share_count: int = Field(title="공유수")
+    create_date: str = Field(title="생성일")
+    modified_date: str = Field(title="수정일")
+
+
+class SearchDto(BaseModel):
+    id: str = Field(title="index id", description="_id")
+    index: str = Field(title="index name", description="_index")
+    type: str = Field(title="index type", description="_type")
+    score: float = Field(title="검색 결과 점수", description="_score")
+    source: Meme = Field(title="밈 데이터", description="_source")
+
+
+def create_index(_index):
+    resp = es.indices.create(
+        index=_index,
+        body={
+            "settings": {
+                "index": {
+                    "analysis": {
+                        "analyzer": {
+                            "korean": {"type": "custom", "tokenizer": "seunjeon"},
+                            "ngram_analyzer": {"tokenizer": "ngram_tokenizer"},
+                        },
+                        "tokenizer": {
+                            "seunjeon": {
+                                "type": "seunjeon_tokenizer",
+                                "index_poses": [
+                                    "UNK",
+                                    "EP",
+                                    "M",
+                                    "N",
+                                    "SL",
+                                    "SH",
+                                    "SN",
+                                    "V",
+                                    "VCP",
+                                    "XP",
+                                    "XS",
+                                    "XR",
+                                ],
+                            },
+                            "ngram_tokenizer": {
+                                "type": "ngram",
+                                "min_gram": "2",
+                                "max_gram": "3",
+                            },
+                        },
+                    },
+                    "max_ngram_diff": "4",
+                }
+            },
+            "mappings": {
+                "properties": {
+                    "title": {
+                        "type": "text",
+                        "analyzer": "korean",
+                        "fields": {
+                            "ngram": {"type": "text", "analyzer": "ngram_analyzer"}
+                        },
+                    },
+                    "tags": {
+                        "type": "text",
+                        "analyzer": "korean",
+                        "fields": {
+                            "ngram": {"type": "text", "analyzer": "ngram_analyzer"}
+                        },
+                    },
+                    "image_url": {"type": "text"},
+                }
+            },
+        },
+    )
+
+    return resp
+
+
+def delete_index(_index):
+    es.indices.delete(index=_index)
+
+
+# print(create_index("meme"))
+
+
+def clean_data(data):
+    data = [d["_source"] for d in data]
+    return data
+
+
+@app.get("/search-page", response_class=HTMLResponse)
+def search(request: Request):
+    return templates.TemplateResponse("search.html", context={"request": request})
+
+
+def get_word_count(data, target_tag):
+    tags = []
+    for d in data:
+        tags.extend(d['tags'])
+    counter_dict = Counter(tags)
+
+    del counter_dict[target_tag]
+    return counter_dict
+
+
+@app.get("/recommend-tags")
+def recommend_tags(tag: str):
+    _index = "meme"
+
+    doc = {
+        "query": {
+            "bool": {
+                "should": [
+                    {"match": {"tags": {"query": tag}}},
+                    {
+                        "bool": {
+                            "should": [
+                                {"match": {"translator": "Constance Garnett"}},
+                                {"match": {"translator": "Louise Maude"}},
+                            ]
+                        }
+                    },
+                ]
+            },
+        }
+    }
+
+    res = es.search(index=_index, body=doc)
+    # print(res['hits']['hits'])
+    data = get_word_count(clean_data(res["hits"]["hits"]), tag)
+    data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
+    result = {"data": data}
+    return JSONResponse(content=result)
+
+
+@app.get(
+    path="/search",
+    description="검색 API",
+    status_code=status.HTTP_200_OK,
+    response_model=Meme,
+    responses={200: {"description": "200 응답 데이터는 data 키 안에 들어있음"}},
+)
+async def search(request: Request, keyword: str, offset: int = 0, limit: int = 30):
+    logger.info(f"[{request.client.host}] keyword: {keyword}")
+
+    _index = "meme"  # index name
+
+    doc = {
+        "query": {
+            "bool": {
+                "should": [
+                    {"match": {"title": {"query": keyword, "operator": "and", "boost": 3}}},
+                    {"match": {"tags": {"query": keyword, "operator": "and", "boost": 3}}},
+                    {"match": {"title": {"query": keyword, "operator": "or"}}},
+                    {"match": {"tags": {"query": keyword, "operator": "or"}}},
+                    {"match_phrase": {"title.ngram": keyword}},
+                    {"match_phrase": {"tags.ngram": keyword}},
+                    {
+                        "bool": {
+                            "should": [
+                                {"match": {"translator": "Constance Garnett"}},
+                                {"match": {"translator": "Louise Maude"}},
+                            ]
+                        }
+                    },
+                ],
+                "minimum_should_match": 1,
+            }
+        },
+        "from": offset,
+        "size": limit,
+        "sort": [{"_score": "desc"}],
+    }
+
+    res = es.search(index=_index, body=doc)
+    # print(res['hits']['hits'])
+    result = {"data": clean_data(res["hits"]["hits"])}
+    return JSONResponse(content=result)
+
+
+@app.get(
+    path="/search/tag",
+    description="태그 검색 API",
+    status_code=status.HTTP_200_OK,
+    response_model=Meme,
+    responses={200: {"description": "200 응답 데이터는 data 키 안에 들어있음"}},
+)
+async def search_by_tag(request: Request, keyword: str, offset: int = 0, limit: int = 30):
+    logger.info(f"[{request.client.host}] keyword: {keyword}")
+
+    _index = "meme"  # index name
+
+    doc = {
+        "query": {
+            "bool": {
+                "should": [
+                    {"match": {"tags": {"query": keyword}}},
+                    {
+                        "bool": {
+                            "should": [
+                                {"match": {"translator": "Constance Garnett"}},
+                                {"match": {"translator": "Louise Maude"}},
+                            ]
+                        }
+                    },
+                ]
+            }
+        },
+        "from": offset,
+        "size": limit,
+        "sort": [{"_score": "desc"}],
+    }
+
+    res = es.search(index=_index, body=doc)
+    # print(res['hits']['hits'])
+    result = {"data": clean_data(res["hits"]["hits"])}
+    return JSONResponse(content=result)
+
+
+@app.get(path="/log-viewer")
+async def log_viewer(request: Request):
+    return templates.TemplateResponse("log_viewer.html", context={"request": request})
+
+
+@app.get(path="/log")
+async def get_logs(request: Request):
+    logs = []
+
+    dir_path = "./logs/"
+    for path in os.listdir(dir_path):
+        with open(dir_path + path,"rt") as f:
+            lines = f.readlines()
+            for line in lines:
+                logs.append(line)
+
+    return logs
---
+++ fastapi_project/ocr_text.py
@@ -0,0 +1,126 @@
+from google.cloud import vision
+from konlpy.tag import Okt
+import os
+import io
+import json
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "meme-369708-e4bd2f8056f2.json"
+
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+# file_name = os.path.abspath('./img/263A4D3D55B585AC0C.jpeg')
+
+# # Loads the image into memory
+# with io.open(file_name, 'rb') as image_file:
+#     content = image_file.read()
+
+# image = vision.Image(content=content)
+
+# Performs label detection on the image file
+dir_path = "./crawling_images/crawling_images/2022125_12958_박명수 짤/"
+
+# list to store files
+res = []
+
+# Iterate directory
+for path in os.listdir(dir_path):
+    print(path)
+
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        res.append(path)
+
+for r in res:
+    with io.open(dir_path + r, "rb") as image_file:
+        content = image_file.read()
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    okt = Okt()
+    print(
+        r,
+        list(
+            filter(
+                lambda x: len(x) > 1,
+                okt.nouns(response.text_annotations[0].description),
+            )
+        ),
+    )
+
+
+def gc_logo():
+    response = client.logo_detection(image=image)
+    logos = response.logo_annotations
+    print("Logos:")
+
+    for logo in logos:
+        print(logo.description)
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+
+
+# gc_logo()
+
+
+def gc_web():
+    response = client.web_detection(image=image)
+    annotations = response.web_detection
+
+    if annotations.best_guess_labels:
+        for label in annotations.best_guess_labels:
+            print("\nBest guess label: {}".format(label.label))
+
+    if annotations.pages_with_matching_images:
+        print(
+            "\n{} Pages with matching images found:".format(
+                len(annotations.pages_with_matching_images)
+            )
+        )
+
+        for page in annotations.pages_with_matching_images:
+            print("\n\tPage url   : {}".format(page.url))
+
+            if page.full_matching_images:
+                print(
+                    "\t{} Full Matches found: ".format(len(page.full_matching_images))
+                )
+
+                for image in page.full_matching_images:
+                    print("\t\tImage url  : {}".format(image.url))
+
+            if page.partial_matching_images:
+                print(
+                    "\t{} Partial Matches found: ".format(
+                        len(page.partial_matching_images)
+                    )
+                )
+
+                for image in page.partial_matching_images:
+                    print("\t\tImage url  : {}".format(image.url))
+
+    if annotations.web_entities:
+        print("\n{} Web entities found: ".format(len(annotations.web_entities)))
+
+        for entity in annotations.web_entities:
+            print("\n\tScore      : {}".format(entity.score))
+            print("\tDescription: {}".format(entity.description))
+
+    if annotations.visually_similar_images:
+        print(
+            "\n{} visually similar images found:\n".format(
+                len(annotations.visually_similar_images)
+            )
+        )
+
+        for image in annotations.visually_similar_images:
+            print("\tImage url    : {}".format(image.url))
+
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
